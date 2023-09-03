9385de4a42044a14c51b3c25b8567cf4a52b754b
Juyeong Maing <mjy9088@naver.com>
2023-09-04 02:20:28 +0900

    initial commit

---
+++ bin.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+const fs = require('fs');
+
+const {
+  name,
+  version,
+  description,
+  types,
+  main,
+  bin,
+  author,
+  repository,
+  license,
+  peerDependencies,
+  dependencies,
+} = JSON.parse(fs.readFileSync('./package.json').toString());
+
+fs.writeFileSync(
+  './dist/package.json',
+  JSON.stringify({
+    name,
+    version,
+    description,
+    types,
+    main,
+    bin,
+    author,
+    repository,
+    license,
+    peerDependencies,
+    dependencies,
+  })
+);
