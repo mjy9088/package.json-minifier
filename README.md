df1831ae9777846f109b7c5fb39cde808b223240
Juyeong Maing <mjy9088@naver.com>
2023-09-04 23:15:46 +0900

    add all required fields

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # `package.json` minifier
 
-usage: `npx package-json-minifier`
+usage: `npx package.json-minifier`
 
 it will minify the `package.json` file info `dist/package.json`.
 
-## IMPORTANT NOTE: This package is under development.
-
-**NOT FUNCTIONAL YET**
+_**`files` field will be removed**_ because it assumes every required files will be copied into `dist/` directory.
--- bin.js
+++ bin.js
@@ -2,33 +2,75 @@
 
 const fs = require('fs');
 
-const {
+function extractGitHubRepo(inputString) {
+  const regex = /^https:\/\/github\.com\/([^/]+)\/([^/]+)\.git$/;
+  const match = inputString?.match(regex);
+
+  if (match) {
+    const username = match[1];
+    const repository = match[2];
+    return `github:${username}/${repository}`;
+  } else {
+    return inputString;
+  }
+}
+
+const minify = ({
+  description,
   name,
   version,
-  description,
-  types,
-  main,
-  bin,
-  author,
-  repository,
+  keywords,
+  homepage,
+  bugs,
   license,
-  peerDependencies,
+  author,
+  contributors,
+  funding,
+  // ignore files, because every required files will be copied to dist directory
+  main,
+  browser,
+  bin,
+  man,
+  directories,
+  repository,
   dependencies,
-} = JSON.parse(fs.readFileSync('./package.json').toString());
+  // ignore devDependencies, because it will not be used after publish
+  peerDependencies,
+  peerDependenciesMeta,
+  overrides,
+  engines,
+  os,
+  cpu,
+  // ignore private, publishConfig, because it will not be used after publish
+}) => ({
+  description,
+  name,
+  version,
+  keywords,
+  homepage,
+  bugs,
+  license,
+  author,
+  contributors,
+  funding,
+  main,
+  browser,
+  bin,
+  man,
+  directories,
+  repository: extractGitHubRepo(repository),
+  dependencies,
+  peerDependencies,
+  peerDependenciesMeta,
+  overrides,
+  engines,
+  os,
+  cpu,
+});
 
 fs.writeFileSync(
   './dist/package.json',
-  JSON.stringify({
-    name,
-    version,
-    description,
-    types,
-    main,
-    bin,
-    author,
-    repository,
-    license,
-    peerDependencies,
-    dependencies,
-  })
+  JSON.stringify(
+    minify(JSON.parse(fs.readFileSync('./package.json').toString()))
+  )
 );
